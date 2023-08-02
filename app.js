08f3c9520bd6239e1465c9b09f348a3b468e2bd1
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-08-02 19:49:54 +0530

    routed landing page
    Lading page has been routed to the app.js file

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //importing epxress
 const express = require('express');
 
+//custom importing routes
+const landingPageRouter = require('./routes/landing-page');
+
 const app = express();
 
-//starting middlewares
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome to chat application</h1>')
-});
 
+app.use('/login', landingPageRouter);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Error 404</h1><h1>Page not found</h1>')
+})
 
 //starting sever
 const port = 2000;
---
+++ routes/landing-page.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router.use(bodyParser.urlencoded({extended: true}));
+
+
+const code = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat App</title>
+</head>
+<body>
+    <h1>Welcome to chat app</h1>
+    <h4>Please login to continue</h4>
+    <form action="" method="post" id ="form">
+        <input type="text" name="username" id="username">
+        <button type="submit">Login</button>
+    </form>
+    <script>
+        const myform = document.getElementById('form');
+
+        myform.addEventListener('submit',()=>{
+            const name = document.getElementById('username');
+            console.log(name.value);
+            localStorage.setItem('username', name.value);
+        });
+    </script>
+</body>
+</html>`;
+
+router.get('/',(req,res,next)=>{
+    res.send(code);
+})
+router.post('/',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/user')
+})
+
+module.exports= router;
